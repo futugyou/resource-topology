35425665e8dd9e4a8d3d723f8219daea3b866a79
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-11-02 13:26:43 +0000

    Updtae resource process handle resource ship change

--- src/AwsAgent/Services/ResourceProcessor.cs
+++ src/AwsAgent/Services/ResourceProcessor.cs
@@ -1,13 +1,15 @@
+
 namespace AwsAgent.Services;
 
 public class ResourceProcessor(ILogger<ResourceProcessor> logger, IOptionsMonitor<ServiceOption> optionsMonitor,
-    IResourceRepository resourceRepository, IAMResourceAdapter iamAdapter, DaprClient dapr) : IResourceProcessor
+    IResourceRepository resourceRepository, IResourceRelationshipRepository resourceRelationshipRepository, IAMResourceAdapter iamAdapter, DaprClient dapr) : IResourceProcessor
 {
     public async Task ProcessingData(CancellationToken cancellation)
     {
         var option = optionsMonitor.CurrentValue;
         // 1. get data from db 
         var dbResourcesTask = resourceRepository.ListResources(cancellation);
+        var dbResourceShiplTask = resourceRelationshipRepository.ListResourceRelationships(cancellation);
 
         // 2. get data from aws 
         Task<List<Resource>> awsTask;
@@ -17,32 +19,49 @@ public class ResourceProcessor(ILogger<ResourceProcessor> logger, IOptionsMonito
         }
         else
         {
+            //TODO: how to get relship
             awsTask = iamAdapter.ConvertIAMToResource(cancellation);
         }
 
         // 3. merge data to db
-        await Task.WhenAll(dbResourcesTask, awsTask);
+        await Task.WhenAll(dbResourcesTask, dbResourceShiplTask, awsTask);
         var dbResources = dbResourcesTask.Result;
+        var dbResourceShips = dbResourceShiplTask.Result;
         var awsResources = awsTask.Result;
+        var awsResourceShips = GeneratedAwsResourceShip(awsResources);
 
         var insertDatas = GetExceptDatas(awsResources, dbResources);
         var deleteDatas = GetExceptDatas(dbResources, awsResources);
         var updateDatas = GetIntersectDatas(dbResources, awsResources);
 
-        logger.LogInformation("{count} need to create, {count} need to delete, {count} need to update", insertDatas.Count, deleteDatas.Count, updateDatas.Count);
+        // ship do not need update, only insert and delete
+        var insertShipDatas = GetExceptDatas(awsResourceShips, dbResourceShips);
+        var deleteShipDatas = GetExceptDatas(dbResourceShips, awsResourceShips);
 
-        if (insertDatas.Count == 0 && deleteDatas.Count == 0 && updateDatas.Count == 0)
+        logger.LogInformation("{count} resources need to create, {count} resources need to delete, {count} resources need to update", insertDatas.Count, deleteDatas.Count, updateDatas.Count);
+        logger.LogInformation("{count} ships need to create, {count} ships need to delete,", insertShipDatas.Count, deleteShipDatas.Count);
+
+        if (insertDatas.Count == 0 && deleteDatas.Count == 0 && updateDatas.Count == 0 && insertShipDatas.Count == 0 && deleteShipDatas.Count == 0)
         {
             return;
         }
 
         await resourceRepository.BulkWriteAsync(insertDatas, deleteDatas.Select(p => p.Id).ToList(), updateDatas, cancellation);
-        await dapr.PublishEventAsync("resource-agent", "resources", new ResourceProcessorEvent(insertDatas, deleteDatas.Select(p => p.Id).ToList(), updateDatas), cancellation);
+        await resourceRelationshipRepository.BulkWriteAsync(insertShipDatas, deleteShipDatas.Select(p => p.Id).ToList(), cancellation);
+
+        await dapr.PublishEventAsync("resource-agent", "resources",
+        new ResourceProcessorEvent(insertDatas, deleteDatas.Select(p => p.Id).ToList(), updateDatas, insertShipDatas, deleteShipDatas.Select(p => p.Id).ToList()), cancellation);
     }
 
-    private static List<Resource> GetExceptDatas(List<Resource> first, List<Resource> second)
+    private List<ResourceRelationship> GeneratedAwsResourceShip(List<Resource> awsResources)
     {
-        return first.ExceptBy(second.Select(d => d.Arn), d => d.Arn).ToList();
+        // TODO: fill method
+        return [];
+    }
+
+    private static List<Entity> GetExceptDatas<Entity>(List<Entity> first, List<Entity> second) where Entity : IEntity
+    {
+        return first.ExceptBy(second.Select(d => d.Id), d => d.Id).ToList();
     }
 
     private static List<Resource> GetIntersectDatas(List<Resource> first, List<Resource> second)
