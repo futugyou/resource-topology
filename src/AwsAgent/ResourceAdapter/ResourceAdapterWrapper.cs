7afb844831703cf7bc3221cadd9c427505ec9407
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-11-04 11:08:53 +0000

    Updated ResourceAdapterWrapper

--- src/AwsAgent/ResourceAdapter/ResourceAdapterWrapper.cs
+++ src/AwsAgent/ResourceAdapter/ResourceAdapterWrapper.cs
@@ -5,21 +5,20 @@ public class ResourceAdapterWrapper(IEnumerable<IResourceAdapter> adapters) : IR
 {
     public async Task<(List<Resource>, List<ResourceRelationship>)> GetResourcAndRelationFromAWS(CancellationToken cancellation)
     {
-        var tasks = new List<Task<(List<Resource>, List<ResourceRelationship>)>>();
-        foreach (var adapter in adapters)
-        {
-            tasks.Add(adapter.GetResourcAndRelationFromAWS(cancellation));
-        }
-        await Task.WhenAll(tasks);
+       var tasks = adapters.Select(adapter => adapter.GetResourcAndRelationFromAWS(cancellation)).ToList();
+        
+        // Await all tasks to complete
+        var results = await Task.WhenAll(tasks);
 
         var resources = new List<Resource>();
         var ships = new List<ResourceRelationship>();
-        foreach (var task in tasks)
+        
+        foreach (var (resource, ship) in results)
         {
-            var (resource, ship) = task.Result;
             resources.AddRange(resource);
             ships.AddRange(ship);
         }
+
         return (resources, ships);
     }
 }
