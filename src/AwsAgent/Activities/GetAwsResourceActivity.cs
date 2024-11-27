d225460a85f609b6bfd66f4f571e2d12e3d51864
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-11-27 04:38:23 +0000

    Updated workflow activity

--- src/AwsAgent/Activities/GetAwsResourceActivity.cs
+++ src/AwsAgent/Activities/GetAwsResourceActivity.cs
@@ -1,10 +1,13 @@
 
 namespace AwsAgent.Activities;
 
-public class GetAwsResourceActivity : WorkflowActivity<string, (List<Resource>, List<ResourceRelationship>)>
+public class GetAwsResourceActivity(IResourceAdapterWrapper wrapper) : WorkflowActivity<string, (List<Resource>, List<ResourceRelationship>)>
 {
     public override Task<(List<Resource>, List<ResourceRelationship>)> RunAsync(WorkflowActivityContext context, string input)
     {
-        return Task.FromResult<(List<Resource>, List<ResourceRelationship>)>(([], []));
+        // TODO: dapr workflow does not currently support CancellationToken.
+        // https://github.com/dapr/dotnet-sdk/issues/1225
+        var cancellation = CancellationToken.None;
+        return wrapper.GetResourcAndRelationFromAWS(cancellation);
     }
 }
--- src/AwsAgent/Activities/GetDatabaseResourceActivity.cs
+++ src/AwsAgent/Activities/GetDatabaseResourceActivity.cs
@@ -1,10 +1,17 @@
 
 namespace AwsAgent.Activities;
 
-public class GetDatabaseResourceActivity : WorkflowActivity<string, (List<Resource>, List<ResourceRelationship>)>
+public class GetDatabaseResourceActivity(IResourceRepository resourceRepository, IResourceRelationshipRepository resourceRelationshipRepository)
+: WorkflowActivity<string, (List<Resource>, List<ResourceRelationship>)>
 {
-    public override Task<(List<Resource>, List<ResourceRelationship>)> RunAsync(WorkflowActivityContext context, string input)
+    public override async Task<(List<Resource>, List<ResourceRelationship>)> RunAsync(WorkflowActivityContext context, string input)
     {
-        return Task.FromResult<(List<Resource>, List<ResourceRelationship>)>(([], []));
+        // TODO: dapr workflow does not currently support CancellationToken.
+        // https://github.com/dapr/dotnet-sdk/issues/1225
+        var cancellation = CancellationToken.None;
+        var dbResourcesTask = resourceRepository.ListResourcesAsync(cancellation);
+        var dbResourceShiplTask = resourceRelationshipRepository.ListResourceRelationshipsAsync(cancellation);
+        await Task.WhenAll(dbResourcesTask, dbResourceShiplTask);
+        return (dbResourcesTask.Result, dbResourceShiplTask.Result);
     }
 }
