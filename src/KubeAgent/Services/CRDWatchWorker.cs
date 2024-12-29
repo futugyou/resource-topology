b80b6adf97a122dc597f35ff311ebb5cbb063a65
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-12-29 15:34:00 +0000

    Added hosted service for crd processor

---
+++ src/KubeAgent/Services/CRDWatchWorker.cs
@@ -0,0 +1,33 @@
+
+namespace KubeAgent.Services;
+
+public class CRDWatchWorker(ILogger<CRDWatchWorker> logger, IOptionsMonitor<AgentOptions> optionsMonitor, [FromKeyedServices("CRD")] IResourceProcessor processor) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var serviceOption = optionsMonitor.CurrentValue!;
+            logger.LogInformation("Kube agent crd worker running at: {time}", DateTimeOffset.Now);
+
+            try
+            {
+                await processor.ProcessingData(stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Kube agent crd worker running at: {time}, and get an error: {error}", DateTimeOffset.Now, (ex.InnerException ?? ex).Message);
+            }
+
+            logger.LogInformation("Kube agent crd worker end at: {time}", DateTimeOffset.Now);
+            await Task.Delay(1000 * serviceOption.WorkerInterval, stoppingToken);
+        }
+    }
+
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await processor.Complete(cancellationToken);
+        await base.StopAsync(cancellationToken);
+        logger.LogInformation("Kube agent worker stop at: {time}", DateTimeOffset.Now);
+    }
+}
