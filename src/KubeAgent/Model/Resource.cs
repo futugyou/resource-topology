be2592999a1a6fb4e66ae30b1d94c26c5522ab2c
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-12-29 15:31:13 +0000

    Updated general resource model

--- src/KubeAgent/Model/Resource.cs
+++ src/KubeAgent/Model/Resource.cs
@@ -6,6 +6,8 @@ public class Resource
     public required string ApiVersion { get; set; }
     public required string Kind { get; set; }
     public required string Name { get; set; }
+    public string Group { get; set; } = "";
+    public string Plural { get; set; } = "";
     public required string UID { get; set; }
     public DateTime ResourceCreationTime { get; set; }
     public required string Configuration { get; set; }// json
--- src/KubeAgent/Monitor/CustomResource.cs
+++ src/KubeAgent/Monitor/CustomResource.cs
@@ -1,6 +1,35 @@
 
 namespace KubeAgent.Monitor;
 
+public class GeneralCustomResource : IKubernetesObject<V1ObjectMeta>
+{
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "";
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "";
+    [JsonPropertyName("spec")]
+    public object Spec { get; set; } = new();
+    [JsonPropertyName("status")]
+    public object Status { get; set; } = new();
+    [JsonPropertyName("metadata")]
+    public V1ObjectMeta Metadata { get; set; } = new();
+}
+
+public class Metadata
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = "";
+
+    [JsonPropertyName("namespace")]
+    public string Namespace { get; set; } = "";
+
+    [JsonPropertyName("labels")]
+    public Dictionary<string, string> Labels { get; set; } = [];
+
+    [JsonPropertyName("annotations")]
+    public Dictionary<string, string> Annotations { get; set; } = [];
+}
+
 public abstract class CustomResource : KubernetesObject, IKubernetesObject<V1ObjectMeta>
 {
     [JsonPropertyName("metadata")]
@@ -22,5 +51,5 @@ public abstract class CustomResource<TSpec, TStatus> : CustomResource<TSpec>, IS
 public class CustomResourceList<T> : KubernetesObject where T : IKubernetesObject
 {
     public V1ListMeta Metadata { get; set; } = new();
-    public IList<T> Items { get; set; } = new List<T>();
+    public IList<T> Items { get; set; } = [];
 }
