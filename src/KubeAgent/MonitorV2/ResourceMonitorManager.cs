199378e87f9b58d02d3313cd73acde0d4495bd18
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-12-31 14:41:52 +0000

    Added resource opertae

--- src/KubeAgent/MonitorV2/ResourceMonitorManager.cs
+++ src/KubeAgent/MonitorV2/ResourceMonitorManager.cs
@@ -4,24 +4,35 @@ namespace KubeAgent.MonitorV2;
 public class ResourceMonitorManager(IResourceDiscovery discovery, IResourceMonitor monitor) : IResourceMonitorManager
 {
     private readonly HashSet<string> _currentMonitoredResourceIds = [];
-    
+
     public async Task MonitorResource(CancellationToken cancellation)
     {
         var resources = await discovery.GetMonitoredResourcesAsync(cancellation);
-        var resourceIds = new HashSet<string>(resources.Select(r => r.ID()));
 
-        var newResources = resources.Where(r => !_currentMonitoredResourceIds.Contains(r.ID())).ToList();
-        foreach (var resource in newResources)
+        var resourcesToAdd = resources.Where(r => r.Operate == "add").ToList();
+        var resourcesToDelete = resources.Where(r => r.Operate == "delete").ToList();
+        var resourcesToRestart = resources.Where(r => r.Operate == "restart").ToList();
+
+        foreach (var resource in resourcesToDelete)
         {
+            await monitor.StopMonitoringAsync(resource.ID());
+            _currentMonitoredResourceIds.Remove(resource.ID());
+        }
+
+        foreach (var resource in resourcesToRestart)
+        {
+            await monitor.StopMonitoringAsync(resource.ID());
             await monitor.StartMonitoringAsync(resource);
             _currentMonitoredResourceIds.Add(resource.ID());
         }
 
-        var removedResources = _currentMonitoredResourceIds.Except(resourceIds).ToList();
-        foreach (var resourceId in removedResources)
+        foreach (var resource in resourcesToAdd)
         {
-            await monitor.StopMonitoringAsync(resourceId);
-            _currentMonitoredResourceIds.Remove(resourceId);
+            if (!_currentMonitoredResourceIds.Contains(resource.ID()))
+            {
+                await monitor.StartMonitoringAsync(resource);
+                _currentMonitoredResourceIds.Add(resource.ID());
+            }
         }
     }
 }
