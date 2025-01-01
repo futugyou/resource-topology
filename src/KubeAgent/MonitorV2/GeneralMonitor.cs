7e25c8982dd76fd634c9b27e484730aa1b86c1db
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2025-01-01 05:31:23 +0000

    Updated resources event handling

--- src/KubeAgent/MonitorV2/GeneralMonitor.cs
+++ src/KubeAgent/MonitorV2/GeneralMonitor.cs
@@ -7,7 +7,7 @@ public class GeneralMonitor(ILogger<GeneralMonitor> logger, IKubernetes client,
  [FromKeyedServices("Custom")] IDataProcessor<MonitoredResource> rewatchProcessor, [FromKeyedServices("General")] IDataProcessor<Resource> processor)
 : IResourceMonitor
 {
-    readonly Dictionary<string, Watcher<object>> watcherList = [];
+    readonly Dictionary<string, WatcherInfo> watcherList = [];
     public Task StartMonitoringAsync(MonitoredResource resource, CancellationToken cancellation)
     {
         using var childCts = CancellationTokenSource.CreateLinkedTokenSource(cancellation);
@@ -18,10 +18,13 @@ public class GeneralMonitor(ILogger<GeneralMonitor> logger, IKubernetes client,
         var watcher = resources.Watch(
             onEvent: (Action<WatchEventType, object>)(async (type, item) =>
             {
-                await OnEvent(resource.ReflectionType, type, item, cancellation);
+                await OnEvent(resource, type, item, cancellation);
             }),
             onError: async (ex) =>
             {
+                // From the code of KubernetesClient, 
+                // The loop will stop watching when the stream reading fails. 
+                // The stream reading is successful, but the watching will not be stopped when the processing fails.
                 if (ex is KubernetesException kubernetesError)
                 {
                     if (string.Equals(kubernetesError.Status.Reason, "Expired", StringComparison.Ordinal))
@@ -40,22 +43,44 @@ public class GeneralMonitor(ILogger<GeneralMonitor> logger, IKubernetes client,
                 logger.LogInformation("closed: {group} {version} {plural}", resource.KubeGroup, resource.KubeApiVersion, resource.KubePluralName);
             });
 
-        watcherList[resource.ID()] = watcher;
+        watcherList[resource.ID()] = new() { Watcher = watcher, Resource = resource, LastActiveTime = DateTime.Now }; ;
         return Task.CompletedTask;
     }
 
-    private async Task OnEvent(Type reflectionType, WatchEventType type, object item, CancellationToken cancellation)
+    private async Task OnEvent(MonitoredResource resource, WatchEventType watchEventType, object item, CancellationToken cancellation)
     {
         var json = JsonSerializer.Serialize(item);
-        object? deserializedObject = JsonSerializer.Deserialize(json, reflectionType);
+        object? deserializedObject = JsonSerializer.Deserialize(json, resource.ReflectionType);
         if (deserializedObject is IKubernetesObject<V1ObjectMeta> kubernetesObject)
         {
-            if (type == WatchEventType.Bookmark)
+            logger.LogInformation("event: {type} {kind} {name}", watchEventType, kubernetesObject.Kind, kubernetesObject.Name());
+
+            if (watchEventType == WatchEventType.Error)
             {
                 return;
             }
 
-            Console.WriteLine($"Event: {kubernetesObject.Kind} {kubernetesObject.ApiGroup()} {kubernetesObject.ApiVersion} {kubernetesObject.Name()}");
+            if (watchEventType == WatchEventType.Added ||
+                        watchEventType == WatchEventType.Modified ||
+                        watchEventType == WatchEventType.Deleted ||
+                        watchEventType == WatchEventType.Bookmark)
+            {
+                if (watcherList.TryGetValue(resource.ID(), out var watcher))
+                {
+                    resource.ResourceVersion = kubernetesObject.ResourceVersion();
+                    watcher.Resource = resource;
+                    if (watchEventType != WatchEventType.Bookmark)
+                    {
+                        watcher.LastActiveTime = DateTime.Now;
+                    }
+                }
+            }
+
+            if (watchEventType == WatchEventType.Bookmark)
+            {
+                return;
+            }
+
             if (kubernetesObject.Kind == "CustomResourceDefinition" && kubernetesObject is V1CustomResourceDefinition crd)
             {
                 foreach (var version in crd.Spec.Versions)
@@ -84,7 +109,7 @@ public class GeneralMonitor(ILogger<GeneralMonitor> logger, IKubernetes client,
                     Name = kubernetesObject.Name(),
                     UID = kubernetesObject.Uid(),
                     Configuration = JsonSerializer.Serialize(jsonItem),
-                    Operate = type.ToString(),
+                    Operate = watchEventType.ToString(),
                 };
                 await processor.CollectingData(res, cancellation);
             }
@@ -97,12 +122,19 @@ public class GeneralMonitor(ILogger<GeneralMonitor> logger, IKubernetes client,
 
     public Task StopMonitoringAsync(string resourceId)
     {
-        if (watcherList.TryGetValue(resourceId, out var watcher))
+        if (watcherList.TryGetValue(resourceId, out var watcherInfo))
         {
-            watcher.Dispose();
+            watcherInfo.Watcher?.Dispose();
             watcherList.Remove(resourceId);
         }
         logger.LogInformation("stop monitoring: {resourceId}", resourceId);
         return Task.CompletedTask;
     }
+}
+
+public class WatcherInfo
+{
+    public MonitoredResource? Resource { get; set; }
+    public Watcher<object>? Watcher { get; set; }
+    public DateTime LastActiveTime { get; set; }
 }
