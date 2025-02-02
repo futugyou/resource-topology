7f8adcda1098c647ab68ac8aca3e9ff3f43fa037
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2025-02-02 15:11:29 +0000

    Updated app host added sidecar options

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -2,10 +2,27 @@ using Aspire.Hosting.Dapr;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.AwsAgent>("aws").WithDaprSidecar();
+DaprSidecarOptions awsSidecarOptions = new()
+{
+    AppId = "aws-agent",
+    DaprGrpcPort = 50001,
+    MetricsPort = 9090,
+    LogLevel = "warn",
+    EnableApiLogging = true,
+};
+builder.AddProject<Projects.AwsAgent>("aws-agent").WithDaprSidecar(awsSidecarOptions);
 
 builder.AddProject<Projects.KubeAgent>("k8s");
 
-builder.AddProject<Projects.ResourceManager>("manager").WithDaprSidecar();
+DaprSidecarOptions managerSidecarOptions = new()
+{
+    AppId = "resource-manager",
+    DaprGrpcPort = 50001,
+    AppPort = 5000,
+    MetricsPort = 9090,
+    LogLevel = "warn",
+    EnableApiLogging = true,
+};
+builder.AddProject<Projects.ResourceManager>("resource-manager").WithDaprSidecar(managerSidecarOptions);
 
 builder.Build().Run();
