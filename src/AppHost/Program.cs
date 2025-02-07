8ac9da6627860e737153e66bd0efaa2a3f14651b
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2025-02-07 15:56:28 +0000

    Updated app host use CommunityToolkit dapr package

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -1,9 +1,22 @@
-using Aspire.Hosting.Dapr;
+using CommunityToolkit.Aspire.Hosting.Dapr;
+using Microsoft.Extensions.Configuration;
 
 var builder = DistributedApplication.CreateBuilder(args);
-var daprSupported = builder.AddParameter("DaprSupported");
+var daprSupported = builder.Configuration.GetValue<string>("DaprSupported");
 
 var rabbitmq = builder.AddRabbitMQ("rabbitmq");
+
+var mongo = builder.AddMongoDB("mongo")
+                   .WithLifetime(ContainerLifetime.Persistent);
+
+var awsDB = builder.ExecutionContext.IsRunMode
+    ? mongo.AddDatabase("Mongodb")
+    : builder.AddConnectionString("Mongodb");
+
+var awsProject = builder.AddProject<Projects.AwsAgent>("aws-agent")
+.WithReference(awsDB)
+.WaitFor(awsDB)
+.WithEnvironment("DaprSupported", daprSupported);
 
 DaprSidecarOptions awsSidecarOptions = new()
 {
@@ -14,20 +27,43 @@ DaprSidecarOptions awsSidecarOptions = new()
     EnableApiLogging = true,
 };
 
-var mongo = builder.AddMongoDB("mongo")
-                   .WithLifetime(ContainerLifetime.Persistent);
+if (daprSupported == "true")
+{
+    var awsAgentConfig = builder.AddDaprComponent("aws-agent-config", "configuration");
+    var awsAgentSecret = builder.AddDaprComponent("aws-agent-secret", "secretstores");
+    var awsAgentState = builder.AddDaprStateStore("aws-agent-state");
+    var resourceAgent = builder.AddDaprPubSub("resource-agent");
 
-var awsDB = builder.ExecutionContext.IsRunMode
-    ? mongo.AddDatabase("Mongodb")
-    : builder.AddConnectionString("Mongodb");
+    awsProject.WithDaprSidecar(awsSidecarOptions)
+       .WithReference(awsAgentConfig)
+       .WithReference(awsAgentSecret)
+       .WithReference(awsAgentState)
+       .WithReference(resourceAgent);
+}
 
-builder.AddProject<Projects.AwsAgent>("aws-agent")
-.WithDaprSidecar(awsSidecarOptions)
-.WithReference(awsDB)
-.WaitFor(awsDB)
+var kubeProject = builder.AddProject<Projects.KubeAgent>("k8s")
 .WithEnvironment("DaprSupported", daprSupported);
 
-builder.AddProject<Projects.KubeAgent>("k8s")
+DaprSidecarOptions kubeSidecarOptions = new()
+{
+    AppId = "kube-agent",
+    DaprGrpcPort = 50001,
+    MetricsPort = 9090,
+    LogLevel = "warn",
+    EnableApiLogging = true,
+};
+
+if (daprSupported == "true")
+{
+    kubeProject.WithDaprSidecar(kubeSidecarOptions);
+}
+else
+{
+    kubeProject.WithReference(rabbitmq).WaitFor(rabbitmq);
+}
+
+
+var managerProject = builder.AddProject<Projects.ResourceManager>("resource-manager")
 .WithReference(rabbitmq)
 .WaitFor(rabbitmq)
 .WithEnvironment("DaprSupported", daprSupported);
@@ -41,10 +77,10 @@ DaprSidecarOptions managerSidecarOptions = new()
     LogLevel = "warn",
     EnableApiLogging = true,
 };
-builder.AddProject<Projects.ResourceManager>("resource-manager")
-.WithDaprSidecar(managerSidecarOptions)
-.WithReference(rabbitmq)
-.WaitFor(rabbitmq)
-.WithEnvironment("DaprSupported", daprSupported);
+
+if (daprSupported == "true")
+{
+    managerProject.WithDaprSidecar(managerSidecarOptions);
+}
 
 builder.Build().Run();
