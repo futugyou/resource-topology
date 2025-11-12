21a2f4773036c287ce5f414c76d85c65f822c507
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2025-11-12 04:30:35 +0000

    Remove unused code

--- src/KubeAgent/V1/Processor/IResourceProcessor.cs
+++
@@ -1,8 +0,0 @@
-namespace KubeAgent.V1.Processor;
-
-public interface IResourceProcessor
-{
-    Task CollectingData(Resource data, CancellationToken cancellation);
-    Task ProcessingData(CancellationToken cancellation);
-    Task Complete(CancellationToken cancellation);
-}
